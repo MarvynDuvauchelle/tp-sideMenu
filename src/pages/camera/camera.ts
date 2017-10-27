8f39299270cd24a0a471739be1a9f8368a0d795e
Marvyn <marvyn.duvauchelle@gmail.com>
2017-10-27 10:22:51 +0200

    display img

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -1,20 +1,32 @@
+/*
+Auteur : Marvyn Duvauchelle
+Titre : Fichier controller de la caméra
+Description : Permet de lancer la caméra du télépone et d'enregistrer les photos prises dans la galerie du téléphone.
+*/
+
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+/* Importation du plugin camera */
 import { Camera, CameraOptions } from '@ionic-native/camera';
+/* Importation du plugin base64-to-gallery */
 import { Base64ToGallery } from '@ionic-native/base64-to-gallery';
 
-
+/* Liste des components */
 @Component({
   selector: 'page-camera',
   templateUrl: 'camera.html'
 })
+
 export class CameraPage {
 
+/* Déclaration d'une variable base64 */
     base64Image:String;
 
+/* Déclaration du constructeur, avec en paramètre nos plugins */
   constructor(public navCtrl: NavController, private camera: Camera, private base64ToGallery: Base64ToGallery) {
   }
 
+/* Options pour la caméra */
  options: CameraOptions = {
   quality: 100,
   destinationType: this.camera.DestinationType.DATA_URL,
@@ -22,20 +34,27 @@ export class CameraPage {
   mediaType: this.camera.MediaType.PICTURE
 }
 
+/* Fonction permettant de lancer la caméra */
 runCamera() {
   this.camera.getPicture(this.options).then((imageData) => {
-  // imageData is either a base64 encoded string or a file URI
-  // If it's base64:
+    /* Si la photo a été prise et si elle est encodée en base64 */
+    /* On la stock dans notre variable base64Image */
   this.base64Image = imageData;
+
+  /* On utilise la fonction saveInGallery afin d'enregistrer l'image prise dans la galerie du téléphone */
   this.saveInGallery(this.base64Image);
-  }, (err) => {
-  // Handle error
+}, (err) => {
+    /* Si il y a un soucis, on retourne l'erreur */
   });
 }
 
+/* Fonction permettant de sauvegarder une photo dans la galerie d'un téléphone */
+/* Param : base64Data -> Image encodée en base64 */
 saveInGallery(base64Data) {
     this.base64ToGallery.base64ToGallery(base64Data, { prefix: '_img' }).then(
+        /* Si la photo est bien enregistrée, on affiche un message de succes dans la console */
     res => console.log('Saved image to gallery ', res),
+    /* Si la photo n'est pas enregistrée, on affiche un message d'echec dans la console */
     err => console.log('Error saving image to gallery ', err)
     );
 }
