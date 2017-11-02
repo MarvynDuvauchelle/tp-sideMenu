20fc4f11b3de02ca48850dde476ae22c5e16f557
unknown <marvyn.duvauchelle@ynov.com>
2017-11-02 09:08:09 +0100

    add video

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -31,7 +31,7 @@ export class CameraPage {
   quality: 100,
   destinationType: this.camera.DestinationType.DATA_URL,
   encodingType: this.camera.EncodingType.JPEG,
-  mediaType: this.camera.MediaType.PICTURE
+  mediaType: this.camera.MediaType.ALLMEDIA
 }
 
 /* Fonction permettant de lancer la caméra */
